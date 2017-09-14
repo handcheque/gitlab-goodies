e5025b91d4c4b98146e34317774f1b45039b531a
Christoph Witzany <christoph@web.crofting.com>
2017-09-14 20:19:22 +0200

    change layout to table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,7 +24,7 @@ import { Issue, User } from '../services/gitlab.service';
   /**
    * Our list of styles in our component. We may add more to compose many styles together.
    */
-  styleUrls: [ './home.component.css' ],
+  styleUrls: [ './home.component.scss' ],
   /**
    * Every Angular template is first compiled by the browser before Angular runs it's compiler.
    */
